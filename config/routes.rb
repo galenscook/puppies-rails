68e67494d099afb13d132f22267daea2de4000ab
Galen Cook <galenscook@gmail.com>
2015-09-29 17:18:51 -0700

    Add users and sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get    'signup'  => 'users#new'
+  get    'profile' => 'users#show'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  resources :users
+
+  
+
+  # resource :photos do
+  #   resource :hearts
+  # end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
